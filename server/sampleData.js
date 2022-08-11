96e0deeb1b5fb11fb6e504e19a1053300b2d2e31
Robson Nascimento <robsonnascimento@MacBook-Pro-de-Robson.local>
2022-08-11 12:57:04 -0300

    created schema files

---
+++ server/sampleData.js
@@ -0,0 +1,79 @@
+// Projects
+const projects = [
+    {
+      id: '1',
+      clientId: '1',
+      name: 'eCommerce Website',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+      status: 'In Progress',
+    },
+    {
+      id: '2',
+      clientId: '2',
+      name: 'Dating App',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+      status: 'In Progress',
+    },
+    {
+      id: '3',
+      clientId: '3',
+      name: 'SEO Project',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+      status: 'In Progress',
+    },
+    {
+      id: '4',
+      clientId: '4',
+      name: 'Design Prototype',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+      status: 'Done',
+    },
+    {
+      id: '5',
+      clientId: '5',
+      name: 'Auction Website',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+      status: 'In Progress',
+    },
+  ];
+  
+  // Clients
+  const clients = [
+    {
+      id: '1',
+      name: 'Tony Stark',
+      email: 'ironman@gmail.com',
+      phone: '(31)99999-0001',
+    },
+    {
+      id: '2',
+      name: 'Natasha Romanova',
+      email: 'blackwidow@gmail.com',
+      phone: '(31)99999-0002',
+    },
+    {
+      id: '3',
+      name: 'Robson Nascimento',
+      email: 'robsonrrn@gmail.com',
+      phone: '(31)99583-8110',
+    },
+    {
+      id: '4',
+      name: 'Steve Rogers',
+      email: 'steve@gmail.com',
+      phone: '(31)99999-0004',
+    },
+    {
+      id: '5',
+      name: 'Bruce Banner',
+      email: 'bruce@gmail.com',
+      phone: '(31)99999-0001',
+    },
+  ];
+  
+  module.exports = { projects, clients };  
